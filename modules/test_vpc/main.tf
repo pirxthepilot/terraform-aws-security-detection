40230a0ed27f0c0401a9a45e9211896ea33acb71
Joon Guillen <joon@modulogeek.com>
2019-12-27 23:01:49 -0800

    Code naming tweaks and vpc_test module deprecation

--- modules/test_vpc/main.tf
+++
@@ -1,8 +0,0 @@
-# Create the test VPC and use the default security group
-resource "aws_vpc" "security_test" {
-  cidr_block = var.cidr_block
-
-  tags = {
-    Name = "security_test"
-  }
-}
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output security_group_id {
-  value = module.test_vpc.default_security_group_id
+  value = aws_vpc.security_demo.default_security_group_id
 }
 
 output cloudtrail_s3_bucket_id {
@@ -7,5 +7,5 @@ output cloudtrail_s3_bucket_id {
 }
 
 output security_group_event_topic_arn {
-  value = module.security_group_event.sns_topic_arn
+  value = module.sg_ingress_rule_event.sns_topic_arn
 }
